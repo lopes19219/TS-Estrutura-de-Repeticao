7f30960349b8fa433f98bd6bb45c7cbe6d630ff7
lopes19219 <gcjlopes@gmail.com>
2026-09-14 09:17:26 -0300

    commit 1
    Added output examples for classificarConsumo function.

--- Q01.ts
+++ Q01.ts
@@ -15,3 +15,10 @@ console.log("100 -> " + classificarConsumo(100));
 console.log("101 -> " + classificarConsumo(101));
 console.log("220 -> " + classificarConsumo(220));
 console.log("221 -> " + classificarConsumo(221));
+
+/* Saídas:
+0 -> Até 100 kWh
+100 -> Até 100 kWh
+101 -> 101 a 220 kWh
+220 -> 101 a 220 kWh
+221 -> Acima de 220 kWh */
