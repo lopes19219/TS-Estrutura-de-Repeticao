af219110991aa14c59250306a2ad6dc3fa78856a
lopes19219 <gcjlopes@gmail.com>
2026-09-14 09:31:44 -0300

    commit 4

---
+++ Q04.ts
@@ -0,0 +1,22 @@
+type Perfil = "aluno" | "professor" | "coordenador";
+
+function controlarAcesso(perfil: string): string {
+    switch (perfil as Perfil) {
+        case "aluno":
+            return "Acesso liberado para a área do aluno.";
+
+        case "professor":
+            return "Acesso liberado para a área do professor.";
+
+        case "coordenador":
+            return "Acesso liberado para a área do coordenador.";
+
+        default:
+            return "Perfil desconhecido. Nenhuma permissão concedida.";
+    }
+}
+
+console.log(controlarAcesso("aluno"));
+console.log(controlarAcesso("professor"));
+console.log(controlarAcesso("coordenador"));
+console.log(controlarAcesso("visitante")); 
