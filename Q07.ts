385e8e348672175f22aa7fe7484f4c4bc188f6e5
lopes19219 <gcjlopes@gmail.com>
2026-09-16 19:03:52 -0300

    Fix formatting in Q07.ts output comments

--- Q07.ts
+++ Q07.ts
@@ -12,3 +12,10 @@ console.log("1900 -> " + anoBissexto(1900));
 console.log("2000 -> " + anoBissexto(2000));
 console.log("2024 -> " + anoBissexto(2024));
 console.log("2100 -> " + anoBissexto(2100));
+
+/* Saída: 
+
+1900 -> Não é bissexto
+2000 -> Bissexto
+2024 -> Bissexto
+2100 -> Não é bissexto */
