16ae84784422826d580a10ac79292724739a19bf
lopes19219 <gcjlopes@gmail.com>
2026-09-16 18:40:58 -0300

    commit 7

---
+++ Q07.ts
@@ -0,0 +1,14 @@
+function anoBissexto(ano: number): string {
+
+    if ((ano % 4 === 0 && ano % 100 !== 0) || ano % 400 === 0) {
+        return "Bissexto";
+    } else {
+        return "Não é bissexto";
+    }
+}
+
+// Testes 
+console.log("1900 -> " + anoBissexto(1900));
+console.log("2000 -> " + anoBissexto(2000));
+console.log("2024 -> " + anoBissexto(2024));
+console.log("2100 -> " + anoBissexto(2100));
